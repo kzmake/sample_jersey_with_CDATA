d223752df91e61c152b8cbbbc5467449a0e5ea8b
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-10-26 08:14:10 +0900

    :recycle: reformat

--- src/main/java/com/cdata/sample/CdataCharacterEscapeHandler.java
+++ src/main/java/com/cdata/sample/CdataCharacterEscapeHandler.java
@@ -8,23 +8,26 @@ import java.io.Writer;
 
 public class CdataCharacterEscapeHandler implements CharacterEscapeHandler {
 
-        @Override
-        public void escape(char[] buf, int start, int length, boolean isAttVal, Writer out)
-                throws IOException {
-                StringWriter buffer = new StringWriter();
-                for (int i = start; i < start + length; i++) {
-                        buffer.write(buf[i]);
-                }
-
-                String line = buffer.toString();
-                if (!(line.matches("^<!\\[CDATA\\[(.|[\\n\\r\\u2028\\u2029\\u0085])*\\]\\]>$"))) {
-                        line = line.replace("&", "&amp;")
-                                .replace("<", "&lt;")
-                                .replace(">", "&gt;")
-                                .replace("'", "&apos;")
-                                .replace("\"", "&quot;");
-
-                }
-                out.write(line);
+    @Override
+    public void escape(char[] buf, int start, int length, boolean isAttVal, Writer out)
+            throws IOException {
+        StringWriter buffer = new StringWriter();
+        for (int i = start; i < start + length; i++) {
+            buffer.write(buf[i]);
         }
+
+        String line = buffer.toString();
+        if (!(line.matches("^<!\\[CDATA\\[(.|[\\n\\r\\u2028\\u2029\\u0085])*\\]\\]>$"))) {
+            line = line.replace("&", "&amp;")
+                    .replace("<", "&lt;")
+                    .replace(">", "&gt;")
+                    .replace("'", "&apos;")
+                    .replace("\"", "&quot;");
+
+        }
+
+        System.out.println(line);
+
+        out.write(line);
+    }
 }
