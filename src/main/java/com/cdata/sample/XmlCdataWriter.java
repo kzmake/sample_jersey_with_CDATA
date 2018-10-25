3c82ee841ccbab6422047966a4e2910de4b573a3
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-10-26 08:15:07 +0900

    :recycle: Writer検証

--- src/main/java/com/cdata/sample/XmlCdataWriter.java
+++ src/main/java/com/cdata/sample/XmlCdataWriter.java
@@ -1,7 +1,6 @@
 package com.cdata.sample;
 
 import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
@@ -12,6 +11,7 @@ import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,35 +21,41 @@ import java.lang.reflect.Type;
 
 @Provider
 @Singleton
-@Produces(value = MediaType.APPLICATION_XML)
+@Produces(value = {MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class XmlCdataWriter implements MessageBodyWriter {
 
-        @Override
-        public boolean isWriteable(Class type, Type genericType, Annotation[] annotations,
-                MediaType mediaType) {
-                return type.isAnnotationPresent(XmlRootElement.class);
+    @Override
+    public boolean isWriteable(Class type, Type genericType, Annotation[] annotations,
+                               MediaType mediaType) {
+        System.out.println(type);
+        System.out.println(genericType);
+        System.out.println(mediaType);
+        System.out.println(annotations);
+        System.out.println(type.isAnnotationPresent(XmlRootElement.class));
+        System.out.println(type.isAnnotationPresent(XmlAccessorType.class));
+        return type.isAnnotationPresent(XmlAccessorType.class);
+    }
+
+    @Override
+    public void writeTo(Object target, Class type, Type genericType, Annotation[] annotations,
+                        MediaType mediaType, MultivaluedMap httpHeaders, OutputStream entityStream)
+            throws IOException, WebApplicationException {
+
+        try {
+            Marshaller m = JAXBContext.newInstance(type).createMarshaller();
+            m.setProperty("com.sun.xml.bind.marshaller.CharacterEscapeHandler", new CdataCharacterEscapeHandler());
+            m.marshal(target, entityStream);
+        } catch (JAXBException e) {
+            throw new ProcessingException(
+                    "Error serializing a MyBean to the output stream", e);
         }
 
-        @Override
-        public void writeTo(Object target, Class type, Type genericType, Annotation[] annotations,
-                MediaType mediaType, MultivaluedMap httpHeaders, OutputStream entityStream)
-                throws IOException, WebApplicationException {
+    }
 
-                try {
-                        Marshaller m = JAXBContext.newInstance(type).createMarshaller();
-                        m.setProperty("com.sun.xml.bind.marshaller.CharacterEscapeHandler", new CdataCharacterEscapeHandler());
-                        m.marshal(target, entityStream);
-                } catch (JAXBException e) {
-                        throw new ProcessingException(
-                                "Error serializing a MyBean to the output stream", e);
-                }
-
-        }
-
-        @Override
-        public long getSize(Object o, Class type, Type genericType, Annotation[] annotations,
-                MediaType mediaType) {
-                return -1;
-        }
+    @Override
+    public long getSize(Object o, Class type, Type genericType, Annotation[] annotations,
+                        MediaType mediaType) {
+        return -1;
+    }
 
 }
